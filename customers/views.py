d56ff8818802ee9a8e5894bb4497b45c3e80b08b
oramirezperera <ramirez.orlando17@gmail.com>
2026-03-30 22:19:05 -0400

    Changes to views, added the edit customer

--- customers/views.py
+++ customers/views.py
@@ -35,7 +35,17 @@ def customer_create(request):
         customer = form.save()
         messages.success(request, "Customer Created")
         return redirect("customers:detail", pk=customer.pk)
-    return render(request, "customers/form.html", {"form": form, "mode":"edit", "customer": customer})
+    return render(request, "customers/form.html", {"form": form, "mode":"create"})
+
+
+def customer_edit(request, pk: int):
+    customer = get_object_or_404(Customer, pk=pk)
+    form = CustomerForm(request.POST or None, instance=customer)
+    if request.method == "POST" and form.is_valid():
+        form.save()
+        messages.success(request, "Customer updated")
+        return redirect("customers:detail", pk=customer.pk)
+    return render(request, "customers/form.html", {"form": form, "mode":"edit", "customer":customer})
 
 
 def customer_detail(request, pk: int):
